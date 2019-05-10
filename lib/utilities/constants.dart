e02b14044dbbc07b95ec7ce5a764b3f11071bdf6
aakritikishore <aakritikishore@codaemonsoftwares.com>
2019-05-10 16:15:41 +0530

    ios test version

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -6,8 +6,8 @@ import 'package:ugo_flutter/utilities/secrets.dart';
 const String API_HOST = "stage.ugollc.com";
 const String apiVersion = "api/v1/";
 const String basicAuthToken = "Basic VWdvQXV0aDMyMTY1NDpSVGdPQnN0QUJ4MjN4OTgxd3BvQQ==";
-const String CURRENT_VERSION = "3.0.2";
-const String PLATFORM = "android";
+const String CURRENT_VERSION = "3.0.5";
+const String PLATFORM = "ios";
 //const String PLATFORM = "ios";
 
 const double TAX_RATE = 0.09;
