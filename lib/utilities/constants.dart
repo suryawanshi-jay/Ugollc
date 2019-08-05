69a75d8cd1310bc6b38f585c80ee485a7247f63c
aakritikishore <aakritikishore@codaemonsoftwares.com>
2019-08-05 13:51:47 +0530

    ios release

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,14 +1,14 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:ugo_flutter/utilities/secrets.dart';
-//const String API_HOST = "ugollc.com";
-const String API_HOST = "stage.ugollc.com";
+const String API_HOST = "ugollc.com";
+//const String API_HOST = "stage.ugollc.com";
 const String apiVersion = "api/v1/";
 const String basicAuthToken = "Basic VWdvQXV0aDMyMTY1NDpSVGdPQnN0QUJ4MjN4OTgxd3BvQQ==";
-//const String PLATFORM = "ios";
-//const String CURRENT_VERSION = "3.0.6";
-const String PLATFORM = "android";
-const String CURRENT_VERSION = "3.0.2";
+const String PLATFORM = "ios";
+const String CURRENT_VERSION = "3.0.7";
+//const String PLATFORM = "android";
+//const String CURRENT_VERSION = "3.0.2";
 
 const double TAX_RATE = 0.09;
 
